1bb62d05cff7d89ba356ce82cb8da20f08591bd9
Felipe Melchior <fehmel@gmail.com>
2020-02-14 02:50:43 -0300

    feat: add basic operations

--- U_Calculadora.pas
+++ U_Calculadora.pas
@@ -27,13 +27,14 @@ type
     btn_slash: TButton;
     box_buttons: TLayout;
     boxheader: TLayout;
-    Edit1: TEdit;
+    txt_number1: TEdit;
     lb_operator: TLabel;
-    Edit2: TEdit;
+    txt_number2: TEdit;
     procedure btn_minusClick(Sender: TObject);
     procedure btn_plusClick(Sender: TObject);
     procedure btn_timesClick(Sender: TObject);
     procedure btn_slashClick(Sender: TObject);
+    procedure btn_equalClick(Sender: TObject);
   private
     { Private declarations }
   public
@@ -47,24 +48,75 @@ implementation
 
 {$R *.fmx}
 
+procedure TForm1.btn_equalClick(Sender: TObject);
+  var number1, number2: double;
+
+begin
+  number1 := strToFloat(txt_number1.Text);
+  number2 := strToFloat(txt_number2.Text);
+
+  if(lb_operator.Text = '+') then
+  begin
+    showMessage(floatToStr(number1 + number2));
+  end;
+
+  if(lb_operator.Text = '-') then
+  begin
+    showMessage(floatToStr(number1 - number2));
+  end;
+
+  if(lb_operator.Text = '*') then
+  begin
+    showMessage(floatToStr(number1 * number2));
+  end;
+
+
+  if(lb_operator.Text = '/') then
+  begin
+    showMessage(floatToStr(number1 / number2));
+  end;
+end;
+
 procedure TForm1.btn_minusClick(Sender: TObject);
 begin
   lb_operator.Text := '-';
+
+  btn_plus.Enabled := true;
+  btn_minus.Enabled := false;
+  btn_times.Enabled := true;
+  btn_slash.Enabled := true;
 end;
 
 procedure TForm1.btn_plusClick(Sender: TObject);
 begin
   lb_operator.Text := '+';
+
+  btn_plus.Enabled := false;
+  btn_minus.Enabled := true;
+  btn_times.Enabled := true;
+  btn_slash.Enabled := true;
+
 end;
 
 procedure TForm1.btn_slashClick(Sender: TObject);
 begin
   lb_operator.Text := '/';
+
+  btn_plus.Enabled := true;
+  btn_minus.Enabled := true;
+  btn_times.Enabled := true;
+  btn_slash.Enabled := false;
 end;
 
 procedure TForm1.btn_timesClick(Sender: TObject);
 begin
   lb_operator.Text := '*';
+
+  btn_plus.Enabled := true;
+  btn_minus.Enabled := true;
+  btn_times.Enabled := false;
+  btn_slash.Enabled := true;
+
 end;
 
 end.
